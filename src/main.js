f1dafd3f581c801d55f20ee6c9af67a20a272866
pilya <ilsaw@yandex.ru>
2020-11-25 19:53:01 +0300

    добавил мод хистори в роутер

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -7,6 +8,26 @@ import vuetify from './plugins/vuetify';
 Vue.config.productionTip = false;
 
 new Vue({
+  created() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyDoT2tWVflKRcS6woVC3Pz3-hFJsz2eyaA',
+      authDomain: 'meetups-board.firebaseapp.com',
+      databaseURL: 'https://meetups-board.firebaseio.com',
+      projectId: 'meetups-board',
+      storageBucket: 'meetups-board.appspot.com',
+      messagingSenderId: '111257465855',
+      appId: '1:111257465855:web:87906478e60ebe1baa2dca',
+    });
+    // firebase.auth().onAuthStateChanged((user) => {
+    //   if (user) {
+    //     this.$store.commit('setUser', { id: user.uid });
+    //     this.$store.dispatch('loadLists');
+    //     this.$store.dispatch('loadTasks');
+    //   } else {
+    //     this.$store.commit('resetStore');
+    //   }
+    // });
+  },
   router,
   store,
   vuetify,
--- src/router/index.js
+++ src/router/index.js
@@ -11,16 +11,14 @@ const routes = [
     component: Home,
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    path: '/signin',
+    name: 'SignIn',
+    component: SignIn,
   },
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes,
 });
 
