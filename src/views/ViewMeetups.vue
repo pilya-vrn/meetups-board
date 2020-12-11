dda58377b90ddc55ea7005411957e0527988466e
pilya <ilsaw@yandex.ru>
2020-12-11 19:29:43 +0300

    добавил отображение одного митапа

--- src/views/ViewMeetups.vue
+++ src/views/ViewMeetups.vue
@@ -5,8 +5,37 @@
       :src="currentMeetup.imgSrc"
       height="200px"
       ></v-img>
-      <v-card-title :title="currentMeetup.title"> {{ title }}
+      <v-card-title> {{ currentMeetup.title }}
+      <v-card-subtitle>
+      {{currentMeetup.location}}
+    </v-card-subtitle>
       </v-card-title>
+      <v-card-actions>
+      <v-btn
+        color="orange lighten-2"
+        text
+      >
+        Explore
+      </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        icon
+        @click="show = !show"
+      >
+        <v-icon>{{ show ? 'mdi-chevron-up' : 'mdi-chevron-down' }}</v-icon>
+      </v-btn>
+      <v-expand-transition>
+      <div v-show="show">
+        <v-divider></v-divider>
+
+        <v-card-text>
+          I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+        </v-card-text>
+      </div>
+    </v-expand-transition>
+    </v-card-actions>
     </v-card>
   </div>
 </template>
@@ -21,6 +50,7 @@ export default {
   data() {
     return {
       currentMeetup: null,
+      show: false,
     }
   },
   computed: {
