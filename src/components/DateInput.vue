6af3dc6dfa53921538e824948f5d058d3a6b13c8
pilya <ilsaw@yandex.ru>
2020-12-26 15:47:41 +0300

    исправил верстку создания митапов

---
+++ src/components/DateInput.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-menu
+        v-model="showMenu"
+        :close-on-content-click="false"
+        offset-y
+        bottom
+        max-width="290px"
+        min-width="290px">
+        <template v-slot:activator="{ on }">
+            <v-text-field v-on="on" :value="value" label="Date" readonly>
+                <v-btn slot="prepend-inner" icon v-on="on">
+                    <v-icon>mdi-calendar</v-icon>
+                </v-btn>
+            </v-text-field>
+        </template>
+        <v-date-picker
+            :value="value"
+            @input="
+                $emit('input', $event);
+                showMenu = false;
+            "
+            no-title/>
+    </v-menu>
+</template>
+
+<script>
+export default {
+    name: 'DateInput',
+    props: {
+        minDate: String,
+        value: String,
+    },
+    data() {
+        return {
+            showMenu: false,
+        };
+    },
+};
+</script>
---
+++ src/components/TimeInput.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-menu
+        :close-on-content-click="false"
+        offset-y
+        bottom
+        @input="menuInputHandler"
+        max-width="290px"
+        min-width="290px">
+        <template v-slot:activator="{ on }">
+            <v-text-field v-on="on" :value="value" label="Date" readonly>
+                <v-btn slot="prepend-inner" icon v-on="on">
+                    <v-icon>mdi-calendar</v-icon>
+                </v-btn>
+            </v-text-field>
+        </template>
+        <v-time-picker
+            no-title
+            ref="timePicker"
+            format="24hr"
+            :value="value"
+            @input="$emit('input', $event)"/>
+    </v-menu>
+</template>
+
+<script>
+export default {
+    name: 'TimeInput',
+    props: {
+        value: String,
+    },
+    methods: {
+        menuInputHandler(isOpened) {
+            if (!isOpened && !this.$refs.timePicker.selectingHour) {
+                this.$refs.timePicker.selectingHour = true;
+            }
+        },
+    },
+};
+</script>
